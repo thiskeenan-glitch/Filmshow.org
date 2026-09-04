241783bb0653499a8b7df01198e5f27507bdd358
thiskeenan-glitch <thiskeenan@gmail.com>
2026-09-04 13:50:49 -0400

    Fix Google service account OAuth grant type

--- lib/google-sheets-filmmakers.ts
+++ lib/google-sheets-filmmakers.ts
@@ -77,7 +77,7 @@ async function getAccessToken() {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams({
-      grant_type: "urn:ietf:params:oauth2:grant-type:jwt-bearer",
+      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
       assertion,
     }),
     cache: "no-store",
